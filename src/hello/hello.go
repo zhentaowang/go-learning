aa628bb10f5105463acf1c4bcfaf7f7d2bad3f1e
王振涛 <wangzhentao@zhiweicloud.com>
2017-03-25 20:02:58 +0800

    day one learning go lang

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"fmt"
-
+	"fmt"	
 	"util"
 )
 
 func main() {
-	fmt.Printf(util.Reverse("!oG ,olleH"))
+	fmt.Println(util.Reverse("!oG ,olleH"))
+	r := []int{41, 24, 76, 11, 45, 64, 21, 69, 19, 36}
+	s := "acbfd"
+	fmt.Println(s)
+	fmt.Println(util.Sort(s))
+	fmt.Println(util.BubbleSort(s))
+	fmt.Println(util.QuickSort(r, 0, len(r)-1))
 }
--- src/util/util.go
+++ src/util/util.go
@@ -9,3 +9,69 @@ func Reverse(s string) string {
 	}
 	return string(r)
 }
+
+//Sort 简单的将字符串按字符顺序排列
+func Sort(s string) string {
+	r := []rune(s)
+	for i :=0; i < len(r)-1; i=i+1 {
+		for j :=i+1; j < len(r); j=j+1 {
+			if r[i] > r[j] {
+				r[i], r[j] = r[j], r[i]
+			}
+		}
+	}
+	return string(r)
+}
+
+func BubbleSort(s string) string{
+
+	values := []rune(s)
+    flag:=true
+    for i:=0;i<len(values)-1;i++{
+        flag=true
+        for j:=0;j<len(values)-i-1;j++{
+            if values[j]>values[j+1]{
+                values[j],values[j+1]=values[j+1],values[j]
+                flag=false
+            }
+        }
+        if flag==true{
+            // 如果已经顺序对了，就不用继续冒泡排序了。
+            break
+        }
+    }
+	return string(values)
+}
+
+//QuickSort 简单的将数字按字符顺序排列
+func QuickSort(values []int, left,right int) []int{
+	
+	temp:=values[left]
+	p:=left
+	i,j:=left,right 
+
+	for i<=j{
+		for j>=p && values[j]>=temp{
+			j--
+		}
+		if j>=p{
+			values[p],values[j]=values[j],values[p]
+			p=j
+		} 
+		for i<=p && values[i]<=temp{
+			i++
+		}
+		if i<=p{
+			values[p],values[i]=values[i],values[p]
+			p=i
+		}
+	}
+	values[p]=temp
+	if p-left>1{
+		QuickSort(values,left,p-1)
+	}
+	if right-p>1{
+		QuickSort(values,p+1,right)
+	} 
+	return values
+}
